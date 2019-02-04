321f9b0557734298753a873290b3041c062c1ae3
OAALILI <oussama.aalili@outlook.com>
2019-02-04 10:18:49 +0100

    Init

---
+++ Maandag2/Model/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maandag2.Model
+{
+    class Player
+    {
+       
+        String testje = "";
+        string tesn = "";
+        int i = 0;
+
+        public int returnIets()
+        {
+            float x = (float)3.5;
+            string naam = "Aydin";
+            string stringtje = string.Format("Hallo {0}", naam);
+            string nieuwStringtje = stringtje + "Aydin";
+
+            int i = 0;
+            return i;
+        }
+    }
+}
